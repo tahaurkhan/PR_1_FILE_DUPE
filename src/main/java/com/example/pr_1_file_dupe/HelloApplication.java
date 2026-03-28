585843db2bfd21a4712753207c93f251df7425c1
tahaur <x.tahaur@gmail.com>
2026-03-28 23:46:57 +0530

    css

--- src/main/java/com/example/pr_1_file_dupe/HelloApplication.java
+++ src/main/java/com/example/pr_1_file_dupe/HelloApplication.java
@@ -11,8 +11,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("/com/example/pr_1_file_dupe/fxml/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/pr_1_file_dupe/fxml/hello-view.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load());
         String css;
