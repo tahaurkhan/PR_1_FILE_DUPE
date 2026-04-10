8bf6486552b2bd874695fe5144e2473efefaefa1
tahaur <x.tahaur@gmail.com>
2026-04-10 14:48:28 +0530

    launcher

--- src/main/java/com/example/pr_1_file_dupe/Launcher.java
+++ src/main/java/com/example/pr_1_file_dupe/Launcher.java
@@ -1,9 +1,16 @@
 package com.example.pr_1_file_dupe;
 
-import javafx.application.Application;
+import com.example.pr_1_file_dupe.service.CLI.CommandHandler;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        if (args.length > 0) {
+            // CLI Mode: If there are arguments, use the CommandHandler
+            CommandHandler cli = new CommandHandler();
+            cli.handle(args);
+        } else {
+            // GUI Mode: If no arguments, launch JavaFX
+            HelloApplication.main(args);
+        }
     }
 }
